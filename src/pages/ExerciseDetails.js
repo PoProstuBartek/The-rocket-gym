434c715beccb55f61ec8988da82aec0b6fa4dfc5
Bartłomiej Szczotkowski <szczotkowski.b@gmail.com>
2022-06-15 12:59:04 +0200

    Add individual excersises card and yt videos from RapidAPI

--- src/pages/ExerciseDetails.js
+++ src/pages/ExerciseDetails.js
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { Box } from '@mui/material'
+import Detail from '../components/Detail'
+import ExerciseVideos from '../components/ExerciseVideos'
+import SimilarExercises from '../components/SimilarExercises'
+
+import { fetchData, exerciseOptions, youtubeOptions } from '../utils/fetchData'
 
 const ExerciseDetails = () => {
+
+  const [exerciseDetail, setExerciseDetail] = useState({})
+  const [exerciseVideos, setExerciseVideos] = useState([])
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com';
+      const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com'
+
+      const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`, exerciseOptions)
+      setExerciseDetail(exerciseDetailData);
+
+      const exerciseVideosData = await fetchData(`${youtubeSearchUrl}/search?query=${exerciseDetailData.name}`, youtubeOptions)
+      setExerciseVideos(exerciseVideosData.contents);
+    }
+
+    fetchExercisesData();
+  }, [id])
+
   return (
-    <div>ExerciseDetails</div>
+    <Box>
+      <Detail exerciseDetail={exerciseDetail}/>
+      <ExerciseVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name}/>
+      <SimilarExercises />
+    </Box>
   )
 }
 
